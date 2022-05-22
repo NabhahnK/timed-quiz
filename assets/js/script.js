fae71f2d5e796fc50972b2ac269e4a5bd7fabd8c
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-05-22 13:13:01 -0700

    started timer when start is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,21 @@
-var time = 1;
+var start = document.getElementById("start");
+var time = 75;
 var score = 0;
 var question = [
     "What is your name?",
     "Is your name Bob?"
 ]
 
-var timer = setInterval(function () {
-    if (time >= 0) {
-        document.getElementById("timer").innerHTML = time;
-        time--;
-    } else {
-        localStorage.setItem(name, score);
-        clearInterval(timer);
-    }
-}, 1000);
+start.addEventListener("click", function() {
+    console.log("test");
+    var timer = setInterval(function () {
+        if (time >= 0) {
+            document.getElementById("timer").innerHTML = time;
+            time--;
+        } else {
+            localStorage.setItem(name, score);
+            clearInterval(timer);
+        }
+    }, 1000);
+});
+
